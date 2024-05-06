96c7862a5f595fba325a7ed7649032ab03f2d326
Mike Noe <technoe@gmail.com>
2024-05-05 19:51:36 -0500

    Adding src

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// main.rs
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mange the name
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
